92a1f0c3d9b3fe82c4ec0dc33b937a77f29d7807
Keith Hall <kingkeith+github@gmail.com>
2015-10-01 15:49:18 +0300

    better grid formatting
    format the grid better when multiple cells in the same row contain
    multiple lines

--- xmlgrid.py
+++ xmlgrid.py
@@ -51,29 +51,6 @@ def addAllChildrenToDictionary(element, headings, values, hierarchy, includeAttr
 	else:
 		recordValue(headings, values, hierarchy, None, element.text)
 
-def displayField(view, edit, value, separator, colsize):
-	"""add text to grid view, lining up spaces if applicable."""
-	if value is None:
-		value = ''
-	if separator == ' ':
-		if '\n' in value:
-			startCol = view.rowcol(view.size())[1]
-			lines = value.split('\n')
-			for index, line in enumerate(lines):
-				append = separator * (colsize - len(line))
-				if index < len(lines) - 1: # don't append a newline to the last line in the value
-					append += '\n'
-				view.insert(edit, view.size(), (separator * (startCol - view.rowcol(view.size())[1])) + line + append) # prepend enough spaces to start at the right column
-			return
-		else:
-			separator = separator * (colsize - len(value))
-	else:
-		quote = '"'
-		# the following text qualification rules and quote doubling are based on recommendations in RFC 4180
-		if quote in value or value.endswith(' ') or value.endswith('\t') or value.startswith(' ') or value.startswith('\t') or '\n' in value or separator in value: # qualify the text in quotes if it contains a quote, starts or ends in whitespace, or contains the separator or a newline
-			value = quote + value.replace(quote, quote + quote) + quote # to escape a quote, we double it up
-	view.insert(edit, view.size(), value + separator)
-
 def isSGML(view):
 	"""return True if the view's syntax is XML."""
 	currentSyntax = view.settings().get('syntax')
@@ -149,6 +126,15 @@ def parseXMLFile(fileRef, includeXMLNSAttributes):
 			hierarchy.pop()
 	return root
 
+def getCSVValue(value, separator):
+	if value is None:
+		value = ''
+	quote = '"'
+	# the following text qualification rules and quote doubling are based on recommendations in RFC 4180
+	if quote in value or value.endswith(' ') or value.endswith('\t') or value.startswith(' ') or value.startswith('\t') or '\n' in value or separator in value: # qualify the text in quotes if it contains a quote, starts or ends in whitespace, or contains the separator or a newline
+		value = quote + value.replace(quote, quote + quote) + quote # to escape a quote, we double it up
+	return value
+
 class XmlToGridCommand(sublime_plugin.TextCommand): #sublime.active_window().active_view().run_command('xml_to_grid')
 	def run(self, edit):
 		sublime.status_message('parsing xml...')
@@ -174,40 +160,74 @@ class XmlToGridCommand(sublime_plugin.TextCommand): #sublime.active_window().act
 			addAllChildrenToDictionary(child, headings, row, [child.tag], includeAttributes)
 			rows.append(row)
 		
-		# determine the size of each column if space is the separator
-		colsizes = {}
-		if separator == ' ':
-			for item in headings:
-				size = len(hierarchyToHeading(item))
-				for row in rows:
-					if item in row.keys():
-						for line in row[item].split('\n'):
-							itemSize = len(line)
-							if itemSize > size:
-								size = itemSize
-				colsizes[item] = size + 1 # add one to ensure that there is a gap between fields
-		
 		# create a new view to write the grid to
 		gridView = self.view.window().new_file()
 		
-		# write the headings
-		for item in headings:
-			if item not in colsizes:
-				colsizes[item] = 0
-			displayField(gridView, edit, hierarchyToHeading(item), separator, colsizes[item])
-		gridView.insert(edit, gridView.size(), '\n')
-		# write the rows
-		for row in rows:
-			for item in headings:
-				if item in row.keys():
-					value = row[item]
-				else:
-					value = ''
-				displayField(gridView, edit, value, separator, colsizes[item])
-			gridView.insert(edit, gridView.size(), '\n')
+		# if grid-mode
+		if separator == ' ':
+			linesForCell = lambda heading, row: row.get(heading, '').split('\n')
+			# determine the contents of each cell in the grid
+			columns = list(map(lambda heading: [[hierarchyToHeading(heading)]] + list(map(lambda row: linesForCell(heading, row), rows)), headings))
+			
+			# determine the size of each row
+			rowSizes = []
+			for column in columns:
+				for rowNumber in range(len(column)):
+					if len(rowSizes) <= rowNumber:
+						rowSizes.append(0)
+					rowSizes[rowNumber] = max(len(column[rowNumber]), rowSizes[rowNumber])
+			
+			# if some cells span multiple lines
+			if max(rowSizes) > 1:
+				# insert line numbers
+				rowNumbers = [['#']]
+				for rowNumber in range(len(rowSizes)):
+					rowNumbers.append([str(rowNumber + 1)])
+				
+				columns[:0] = [rowNumbers]
+			
+			# determine the size of each column
+			colSizes = list(map(lambda column: max(map(lambda cell: max(list(map(lambda line: len(line), cell))), column)) + 1, columns)) # the + 1 is to ensure that there is a gap between fields
+			
+			# determine column start positions
+			index = 0
+			colStarts = []
+			for colsize in colSizes:
+			 	colStarts.append(index)
+			 	index += colsize
+			
+			# write empty spaces for all lines first, they will be replaced later
+			for rowSize in rowSizes:
+				for rowNumber in range(rowSize):
+					gridView.insert(edit, gridView.size(), (' ' * index) + '\n')
+			index += 1 # account for the new line character at the end of each line
+			
+			# write the cells
+			
+			getPoint = lambda columnNumber, rowNumber: (rowNumber * index) + colStarts[columnNumber]
+			
+			for columnNumber in range(len(headings)): # for each column
+				currentRow = 0
+				for rowNumber in range(len(rowSizes)): # for each row
+					rowStart = currentRow
+					linesForCell = columns[columnNumber][rowNumber]
+					for rowLine in linesForCell:
+						point = getPoint(columnNumber, currentRow)
+						gridView.replace(edit, sublime.Region(point, point + len(rowLine)), rowLine)
+						currentRow += 1
+					currentRow = rowStart + rowSizes[rowNumber]
+		# if csv-mode
+		else:
+			# write the headings
+			gridView.insert(edit, gridView.size(), separator.join(map(hierarchyToHeading, headings)) + '\n')
+			
+			# write the rows
+			for row in rows:
+				gridView.insert(edit, gridView.size(), separator.join(map(lambda heading: getCSVValue(row.get(heading, ''), separator), headings)) + '\n')
 		
 		sublime.status_message('')
 	def is_enabled(self):
 		return isSGML(self.view)
 	def is_visible(self):
 		return isSGML(self.view)
+	
